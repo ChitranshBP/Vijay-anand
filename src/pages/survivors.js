8ba66ad4493f94a0c9558109d6c3b619247774d5
ichhakumari <ichha2111@gmail.com>
2025-11-01 16:16:40 +0530

    SURVIVOR comp

--- src/pages/survivors.js
+++ src/pages/survivors.js
@@ -14,8 +14,7 @@ export const survivors = [
   date: '2025-10-14',
   author: 'Dr. Vijay Anand Reddy',
   banners: [
-    '/assets/Events/i-am-a-survivor-hindi-launch-2025/banner1.jpg',
-    '/assets/Events/i-am-a-survivor-hindi-launch-2025/banner2.jpg'
+    '/assets/Events/imseviour-hindi/banner.jpg',
   ],
   description: `Honoured to launch the Hindi edition of *I Am A Survivor* in New Delhi alongside Union Minister Shri G. Kishan Reddy. Grateful to Sri Konda Vishweshwar Reddy for gracing the occasion.
 
@@ -23,9 +22,13 @@ The event was a deeply emotional and fulfilling experience as patients from Delh
 
 #Hope #Courage #Survivor`,
   photos: [
-    '/assets/Events/i-am-a-survivor-hindi-launch-2025/photo1.jpg',
-    '/assets/Events/i-am-a-survivor-hindi-launch-2025/photo2.jpg',
-    '/assets/Events/i-am-a-survivor-hindi-launch-2025/photo3.jpg'
+   '/assets/Events/imseviour-hindi/i1.jpeg',
+      '/assets/Events/imseviour-hindi/i2.jpeg',
+      '/assets/Events/imseviour-hindi/i3.jpeg',
+      '/assets/Events/imseviour-hindi/i4.jpeg',
+      '/assets/Events/imseviour-hindi/i5.jpeg',
+      '/assets/Events/imseviour-hindi/i6.jpeg',
+      '/assets/Events/imseviour-hindi/i7.jpeg',
   ]
 },
 {
@@ -35,8 +38,10 @@ The event was a deeply emotional and fulfilling experience as patients from Delh
   date: '2019-08-14',
   author: 'Dr. Vijay Anand Reddy',
   banners: [
-    '/assets/Events/i-am-a-survivor-telugu-launch-2019/banner1.jpg',
-    '/assets/Events/i-am-a-survivor-telugu-launch-2019/banner2.jpg'
+    '/assets/survivors/Telugu-version-Book-Launch/1.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/2.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/3.jpg',
+    
   ],
   description: `Dr. Vijay Anand Reddy launched the Telugu edition of *I Am A Survivor*, a powerful collection of 108 inspiring stories of cancer survivors drawn from his 25 years of experience as an oncologist.
 
@@ -44,9 +49,45 @@ Each story highlights the courage, resilience, and determination of individuals 
 
 The event celebrated the indomitable human spirit and Dr. Reddy’s mission to spread awareness, empathy, and positivity among cancer patients and their families.`,
   photos: [
-    '/assets/Events/i-am-a-survivor-telugu-launch-2019/photo1.jpg',
-    '/assets/Events/i-am-a-survivor-telugu-launch-2019/photo2.jpg',
-    '/assets/Events/i-am-a-survivor-telugu-launch-2019/photo3.jpg'
+    '/assets/survivors/Telugu-version-Book-Launch/1.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/2.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/3.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/4.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/5.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/6.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/6.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/7.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/8.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/9.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/10.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/11.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/12.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/13.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/14.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/15.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/16.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/17.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/18.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/19.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/20.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/21.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/22.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/23.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/24.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/25.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/26.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/27.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/28.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/29.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/30.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/31.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/32.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/33.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/34.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/35.jpg',
+    '/assets/survivors/Telugu-version-Book-Launch/36.jpg',
+
+
   ]
 },
 {
@@ -56,8 +97,10 @@ The event celebrated the indomitable human spirit and Dr. Reddy’s mission to s
   date: '2017-08-24',
   author: 'Dr. Vijay Anand Reddy',
   banners: [
-    '/assets/Events/i-am-a-survivor-launch-2017/banner1.jpg',
-    '/assets/Events/i-am-a-survivor-launch-2017/banner2.jpg'
+    '/assets/survivors/SURVIVOR-Book-Launch/1.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/3.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/4.jpg',
+    
   ],
   description: `Renowned Oncologist Dr. Vijay Anand Reddy, Director and Senior Consultant at Apollo Cancer Hospital, launched his inspirational book *I Am a Survivor* on 24th August 2017 at Hotel Taj Krishna, Hyderabad.
 
@@ -69,9 +112,36 @@ The event also featured emotional testimonies from survivors, who expressed grat
 
 “What Cancer Cannot Do – It cannot cripple love, It cannot shatter hope, It cannot corrode faith, It cannot silence courage, It cannot invade the soul, It cannot steal eternal life, It cannot conquer the spirit.”`,
   photos: [
-    '/assets/Events/i-am-a-survivor-launch-2017/photo1.jpg',
-    '/assets/Events/i-am-a-survivor-launch-2017/photo2.jpg',
-    '/assets/Events/i-am-a-survivor-launch-2017/photo3.jpg'
+    '/assets/survivors/SURVIVOR-Book-Launch/2.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/3.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/4.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/5.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/6.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/7.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/8.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/9.jpg',
+    // '/assets/survivors/SURVIVOR-Book-Launch/10.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/11.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/12.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/13.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/14.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/15.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/16.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/17.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/18.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/19.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/20.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/21.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/22.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/23.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/24.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/25.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/26.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/27.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/28.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/29.jpg',
+    '/assets/survivors/SURVIVOR-Book-Launch/30.jpg',
+
   ]
 }
 
